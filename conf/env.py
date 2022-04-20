0bfb6023bdcddd35e2331a798bb3ed45560d29da
bin549 <bin549@qq.com>
2022-04-20 16:39:12 +0800

    1

--- conf/env.py
+++ conf/env.py
@@ -2,7 +2,7 @@ import os
 
 DATABASE_ENGINE = "django.db.backends.postgresql"
 DATABASE_NAME =  "stem"
-DATABASE_HOST = "127.0.0.1"
+DATABASE_HOST = "47.106.92.143"
 DATABASE_USER = "postgres"
 DATABASE_PASSWORD = "__2018bb"
 DATABASE_PORT = "5432"
